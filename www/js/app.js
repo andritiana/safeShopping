b67fa4c48d5bc47bc8ded890b392d3ae719ffb30
arandriambololontsoa <andritiana.ran@gmail.com>
2015-10-15 22:46:23 +0200

    Init tabs and the differents page of the application

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,14 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('safeShop', ['ionic'])
 
 .run(function($ionicPlatform) {
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    console.log("IN RUN APP");
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -17,3 +19,40 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/tab/home');
+
+    console.log("IN CONG_FIG APP");
+    $stateProvider
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: './templates/tabs.html'
+      })
+      .state('tab.home', {
+        url: '/home',
+        views: {
+          'tab-home': {
+            templateUrl: './templates/home.html'
+          }
+        }
+      })
+      .state('tab.lists', {
+        url: '/lists',
+        views: {
+          'tab-lists': {
+            templateUrl: './templates/lists.html'
+          }
+        }
+      })
+      .state('tab.settings', {
+        url: '/settings',
+        views: {
+          'tab-settings': {
+            templateUrl: './templates/settings.html'
+          }
+        }
+      });
+});
